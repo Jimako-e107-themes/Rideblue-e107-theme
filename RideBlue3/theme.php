d3f657f0f39c9a1ab71dec2cd631176cfc7870ac
Jimmi08 <janamicek@gmail.com>
2021-06-21 15:39:52 +0200

    user template update

--- RideBlue3/theme.php
+++ RideBlue3/theme.php
@@ -1,107 +1,130 @@
 <?php
 
-if (!defined('e107_INIT'))
-{
-	exit;}
+if (!defined('e107_INIT')) {
+    exit;
+}
 
-// [multilanguage]
 e107::lan('theme');
- 
 
-if(defined("e_PAGE")) {
- if(e_PAGE == "login.php" && !e107::getPref('membersonly_enabled')) {
-	define('e_IFRAME', '0');
- }   
-}
-
-/* override with theme prefs */
-$inlinecss = e107::pref('theme', 'inlinecss', FALSE);
-if ($inlinecss)
-{
-	e107::css("inline", $inlinecss);
-}
-$inlinejs = e107::pref('theme', 'inlinejs');
-if ($inlinejs)
-{
-	e107::js("footer-inline", $inlinejs);
+if (defined('e_PAGE')) {
+    if (e_PAGE == 'login.php' && !e107::getPref('membersonly_enabled')) {
+        define('e_IFRAME', '0');
+    }
 }
 
 class theme implements e_theme_render
 {
+    public function init()
+    {
+        e107::lan('theme');
 
-	public function init()
-	{
+        ////// Your own css fixes ////////////////////////////////////////////////////
+        define('CORE_CSS', false);
+        e107::css('theme', 'e107.css');
 
-		/* HEAD part */
-		e107::meta('viewport', 'width=device-width, initial-scale=1.0');
+        ////// Theme meta tags /////////////////////////////////////////////////////////
+        $this->set_metas();
 
-	    define("CORE_CSS", false);
-		e107::css("theme", "css/style.css");
-    
-		e107::css("theme", "css/main_custom.css");
-		
-		e107::js('theme', "custom.js");
-        
-	}
+        /////// Theme css  /////////////////////////////////////////////////////////////
+        $this->register_css();
 
-	public function tablestyle($caption, $text, $mode = '', $options = array())
-	{
-		$style = varset($options['setStyle'], 'default');
+        /////// Theme js  /////////////////////////////////////////////////////////////
+        $this->register_js();
 
-		if (e_DEBUG)
-		{
-			echo "
-			<!-- tablestyle initial:  style=" . $style . "  mode=" . $mode . "  UniqueId=" . varset($options['uniqueId']) . " -->
-			";
-			echo "\n<!-- \n";
+        /////// Theme fonts ///////////////////////////////////////////////////////////
+        $this->register_fonts();
 
-			echo json_encode($options, JSON_PRETTY_PRINT);
+        /////// Theme icons ///////////////////////////////////////////////////////////
+        $this->register_icons();
 
-			echo "\n-->\n\n";
-		}
-        
-		switch ($id)
-		{
-			case 'nocaption':
-				echo   $text  ;
-			break;
+        $this->getInlineCodes();
+    }
+
+    public function set_metas()
+    {
+        e107::meta('viewport', 'width=device-width, initial-scale=1.0');
+    }
+
+    public function register_css()
+    {
+        e107::css('theme', 'css/e107.css');
+        e107::css('theme', 'css/style.css');
+        e107::css('theme', 'css/main_custom.css');
+    }
+
+    public function register_js()
+    {
+        e107::js('theme', 'custom.js');
+    }
+
+    public function register_fonts()
+    {
+    }
+
+    public function register_icons()
+    {
+    }
+
+    public function getInlineCodes()
+    {
+        /* override with theme prefs */
+        $inlinecss = e107::pref('theme', 'inlinecss', false);
+        if ($inlinecss) {
+            e107::css('inline', $inlinecss);
+        }
+        $inlinejs = e107::pref('theme', 'inlinejs');
+        if ($inlinejs) {
+            e107::js('footer-inline', $inlinejs);
+        }
+    }
+
+    public function tablestyle($caption, $text, $mode = '', $options = array())
+    {
+        $style = varset($options['setStyle'], 'default');
+
+        if (e_DEBUG) {
+            echo '
+			<!-- tablestyle initial:  style='.$style.'  mode='.$mode.'  UniqueId='.varset($options['uniqueId']).' -->
+			';
+            echo "\n<!-- \n";
+
+            echo json_encode($options, JSON_PRETTY_PRINT);
+
+            echo "\n-->\n\n";
         }
 
-		 
-		switch ($style)
-		{
+        switch ($mode) {
+            case 'nocaption':
+                echo   $text  ;
+            break;
+        }
 
-			case 'nocaption':
-			case 'main':            
-				echo   $text  ;
-			break;
+        switch ($style) {
+            case 'nocaption':
+            case 'main':
+                echo   $text  ;
+            break;
 
-			case 'sidebar':
-				echo '
-					<div class="headerbox"><h2>' . $caption . '</h2></div>
-					<div class="insidebox">' . $text . '</div>';
-			break;
-			case 'footerbar':
-				echo '
-					<h3>' . $caption . '</h3>
-					<div class="footerbar">' . $text . '</div>';
-			break;
-			case 'topmenu':
-			case 'bottommenu':
-			case 'full':
-			case 'default':	
-			default:	
-				echo '
-					<h3>' . $caption . '</h3>
-					<div class="footerbar">' . $text . '</div>'; 
-				break;
- 
-		}
-		return;
-	}
-
+            case 'sidebar':
+                echo '
+					<div class="headerbox"><h2>'.$caption.'</h2></div>
+					<div class="insidebox">'.$text.'</div>';
+            break;
+            case 'footerbar':
+                echo '
+					<h3>'.$caption.'</h3>
+					<div class="footerbar">'.$text.'</div>';
+            break;
+            case 'topmenu':
+            case 'bottommenu':
+            case 'full':
+            case 'default':
+            default:
+                echo '
+					<h3>'.$caption.'</h3>
+					<div class="footerbar">'.$text.'</div>';
+                break;
+        }
+        return;
+    }
 }
-
-
- 
- 
